bc89d01aef0ad923348eb1a5d9d70cc6321b4d8d
Zakhar Galanin <zakhargalanin@gmail.com>
2022-07-03 21:02:02 -0700

    Add project files.

---
+++ Lab12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус окружности");
+            double r = Convert.ToDouble(Console.ReadLine());
+            double p = Solution.GetLength(r);
+            double s = Solution.GetArea(r);
+            Console.WriteLine($"Длина окружности - {p:.00}, площадь {s:.00}");
+
+            Console.WriteLine("Введите точку (x,y)");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            if (Solution.GetBelong(x,y,r) == false)
+                Console.WriteLine($"Точка с координатами {x} и {y} - не принадлежит окружности с радиусов {r}");
+            else
+                Console.WriteLine($"Точка с координатами {x} и {y} - принадлежит окружности с радиусов {r}");
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
+    
---
+++ Lab12/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    static class Solution
+    {
+        static public double GetLength(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        public static double GetArea(double r)
+        {
+            return Math.PI * Math.Pow(r, 2);
+        }
+        public static bool GetBelong(double x, double y, double r)
+        {
+            if (Math.Sqrt(x * x + y * y) > r)
+                return false;
+            return true;
+        }
+    }
+}
